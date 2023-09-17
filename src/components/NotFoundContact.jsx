bbcb888ce4310fdbb817d9a759779cf1ab1d768c
Harsh-s0ni <hsoni6954@gmail.com>
2023-09-17 17:57:33 +0530

    image

--- src/components/NotFoundContact.jsx
+++ src/components/NotFoundContact.jsx
@@ -2,7 +2,7 @@ const NotFoundContact = () => {
   return (
       <div className="flex justify-center items-center h-[80vh] gap-4">
           <div>
-            <img src="public\Hands Contact.png" alt="" />
+            <img src=".././Hands Contact.png" alt="" />
           </div>
           <h3 className="text-2xl font-semibold text-white">Contact Not Found</h3>
     </div>
